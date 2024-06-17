cc45fc68ce45b3932c58eec4cb228ef9bf65581b
Marco Torres <marcotv505@gmail.com>
2024-06-17 06:03:26 -0700

    fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,22 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 import store
+
+app = FastAPI()
+
+
+@app.get("/")
+def get_list():
+    return [1, 2, 3, 4]
+
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_contact():
+    return """
+        <h1>Soy una pagina de contact</h1>
+    """
+
 
 def run():
     store.get_categories()
