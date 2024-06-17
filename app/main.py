dc78dee98f2f56d7509c12250da450775ec53e88
Marco Torres <marcotv505@gmail.com>
2024-06-17 05:54:19 -0700

    uso de pandas

--- app/main.py
+++ app/main.py
@@ -2,23 +2,35 @@ import charts
 import read
 import utils
 
+import pandas as pd
+
 
 def run():
-    data = read.read_csv("./data.csv")
-    country = input("Type Country => ")
+    # data = read.read_csv("./data.csv")
+    # labels, values = utils.get_wpp(data)
 
-    result = utils.population_by_country(data, country)
+    # result = utils.population_by_country(data, country)
 
-    if len(result) <= 0:
-        print("No se encontro el pais")
-        return
+    # if len(result) <= 0:
+    #     print("No se encontro el pais")
+    #     return
 
-    country = result[0]
-    keys, values = utils.get_population(country)
-    charts.generate_bar_chart(country["Country/Territory"], keys, values)
+    # country = result[0]
+    # keys, values = utils.get_population(country)
+    # charts.generate_bar_chart(country["Country/Territory"], keys, values)
 
-    labels, values = utils.get_wpp(data)
+    df = pd.read_csv("./data.csv")
+    df = df[df["Continent"] == "South America"]
+
+    labels = df["Country/Territory"].values
+    values = df["World Population Percentage"].values
     charts.generate_pie_chart(labels, values)
+
+    country = input("Type Country => ")
+    df = df[df["Country/Territory"] == country]
+
+    keys, values = utils.get_population(df)
+    charts.generate_bar_chart(df["Country/Territory"].values[0], keys, values)
 
 
 if __name__ == "__main__":
